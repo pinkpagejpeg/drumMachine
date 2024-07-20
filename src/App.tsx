753f789f646a2682ade3e0cad928cf38101b7638
pinkpagejpeg <pinkpagejpeg@gmail.com>
2024-07-20 19:04:55 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+import classes from "./styles/main.module.scss"
+
+const App: FC = () => {
+  return (
+    <div className={classes.container}>
+
+    </div>
+  )
+}
+
+export default App
